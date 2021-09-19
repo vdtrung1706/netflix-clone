bb592c1ac86947fe009bfddc6a833570afa4eb9f
Trung Vu <trunpyon.it@gmail.com>
2021-09-19 23:58:14 +0700

    update my list page

--- src/pages/MyListPage.jsx
+++ src/pages/MyListPage.jsx
@@ -1,13 +1,32 @@
 import Layout from '@components/common/Layout';
+import SliderItem from '@components/layout/content/Slider/SliderItem';
+import { selectMyList } from '@store/user-lists/selectors.user-lists';
+import { defaultPageFadeInVariants } from '@utils/motion.utils';
+import { motion } from 'framer-motion';
+import { useSelector } from 'react-redux';
 
 const MyListPage = () => {
+  const myList = useSelector(selectMyList);
+
   return (
     <Layout>
-      <div className="flex flex-col">
-        <div name="slidersWrapper" className="pt-16 slider-wrapper">
-          My list goes here
+      <motion.div
+        variants={defaultPageFadeInVariants}
+        initial="initial"
+        animate="animate"
+        className="mt-20 px-4%"
+      >
+        <div className="font-bold md:text-sm lg:text-base xl:text-xl">
+          My List
         </div>
-      </div>
+        <div className="flex flex-wrap items-centers">
+          {myList.map((movie) => {
+            return (
+              <SliderItem key={movie.id} movie={movie} inSearchPage={true} />
+            );
+          })}
+        </div>
+      </motion.div>
     </Layout>
   );
 };
